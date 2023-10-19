cd77ce7b10f41b02b7a09730bb70655f69a3d9c4
drashtijaviya11 <drashti@biz1.co.il>
2023-10-19 18:05:59 +0530

    responsive pges

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const mobileHeader1 = document.querySelector(".mobile-header-1");
 const lImage = document.querySelector(".l-image");
 const faceLoginTable = document.querySelector(".face-login-table");
 const loginTable = document.querySelector(".login-table");
+const helpButton = document.querySelector(".help-b");
+const profileButton = document.querySelector(".profile-b");
+const signButton = document.querySelector(".signup-b");
+const loginImage = document.querySelector(".l-image");
+const signImage = document.querySelector(".s-image");
+const heroSection = document.querySelector(".hero-section")
 
 let isMenuOpen = false;
 
@@ -27,6 +33,7 @@ window.addEventListener("scroll", () => {
 
 menuBtn.addEventListener("click", () => {
     headerContainer.classList.add("active");
+    heroSection.classList.remove("active");
     isMenuOpen = true;
 });
 
@@ -35,4 +42,57 @@ closeBtn.addEventListener("click", () => {
     isMenuOpen = false;
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Add an event listener to the button with class "help-b"
+    const helpButton = document.querySelector('.help-b');
+    if (helpButton) {
+        helpButton.addEventListener('click', function() {
+            // Navigate to the FAQ page
+            window.location.href = 'faq.html';
+        });
+    }
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+    // Add an event listener to the button with class "help-b"
+    const profileButton = document.querySelector('.profile-b');
+    if (profileButton) {
+        profileButton.addEventListener('click', function() {
+            // Navigate to the FAQ page
+            window.location.href = 'login.html';
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add an event listener to the button with class "help-b"
+    const signButton = document.querySelector('.signup-b');
+    if (signButton) {
+        signButton.addEventListener('click', function() {
+            // Navigate to the FAQ page
+            window.location.href = 'signup.html';
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add an event listener to the button with class "help-b"
+    const loginImage = document.querySelector('.l-image');
+    if (loginImage) {
+        loginImage.addEventListener('click', function() {
+            // Navigate to the FAQ page
+            window.location.href = 'face-login.html';
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Add an event listener to the button with class "help-b"
+    const signImage = document.querySelector('.s-image');
+    if (signImage) {
+        signImage.addEventListener('click', function() {
+            // Navigate to the FAQ page
+            window.location.href = 'face-signup.html';
+        });
+    }
+});
