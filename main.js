8f4fb590b56882aec6e68b5e4cca1281ddc4b955
drashtijaviya11 <drashti@biz1.co.il>
2023-11-09 12:59:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const header = document.querySelector(".header");
+    const header = document.querySelector(".header");
 const mobileHeader = document.querySelector(".mobile-header");
 const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
